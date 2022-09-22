c8838c64f7fa9612fd18c4668438a6797050d48b
f1z1x <104532312+f1z1x@users.noreply.github.com>
2022-09-22 22:43:09 +0200

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -8,6 +8,6 @@ command_names = ['help', 'crash'] # list of commands that trigger crash
 
 channels_names = 'server-crashed' # the name of the channels that will be created during the crash
 
-roles_names = 'Server Crashed' # the name of the channels that will be created during the crash
+roles_names = 'Server Crashed' # the name of the roles that will be created during the crash
 
 spam_message = '@everyone\nServer crashed\nCrash bot by <https://github.com/f1z1x/discord-crash-bot>' # the message that will be spammed to all channels
